356b1449ce6cff55f3f5214edeb89d565df27681
vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2018-03-04 23:23:23 +0000

    add maintenance section, run from github issues

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	var uptimeConfig = {
 		uptimerobot: {
 			api_keys: [
@@ -22,23 +22,23 @@ $(document).ready(function() {
 	};
 
 	var monitors = uptimeConfig.uptimerobot.api_keys;
-	for( var i in monitors ){
+	for (var i in monitors) {
 		var api_key = monitors[i];
 		$.post('https://api.uptimerobot.com/v2/getMonitors', {
 			"api_key": api_key,
 			"format": "json",
 			"logs": uptimeConfig.uptimerobot.logs,
 			"response_times": uptimeConfig.uptimerobot.response_times,
-		}, function(response) {
-			status( response );
+		}, function (response) {
+			status(response);
 		}, 'json');
 	}
 
 	function status(data) {
-		data.monitors = data.monitors.map(function(check) {
+		data.monitors = data.monitors.map(function (check) {
 			check.class = check.status === 2 ? 'label-success' : 'label-danger';
 			check.text = check.status === 2 ? 'operational' : 'major outage';
-			if( check.status !== 2 && !check.lasterrortime ){
+			if (check.status !== 2 && !check.lasterrortime) {
 				check.lasterrortime = Date.now();
 			}
 			if (check.status === 2 && Date.now() - (check.lasterrortime * 1000) <= 86400000) {
@@ -48,27 +48,27 @@ $(document).ready(function() {
 			return check;
 		});
 
-		var status = data.monitors.reduce(function(status, check) {
+		var status = data.monitors.reduce(function (status, check) {
 			return check.status !== 2 ? 'danger' : 'operational';
 		}, 'operational');
-		
+
 		if (!$('#panel').data('incident')) {
-			$('#panel').attr('class', (status === 'operational' ? 'panel-success' : 'panel-warning') );
+			$('#panel').attr('class', (status === 'operational' ? 'panel-success' : 'panel-warning'));
 			$('#paneltitle').html(status === 'operational' ? 'All systems are operational.' : 'One or more systems inoperative');
 		}
-		data.monitors.forEach(function(item) {
+		data.monitors.forEach(function (item) {
 			var name = item.friendly_name;
 			var clas = item.class;
 			var text = item.text;
 			var cleanName = item.friendly_name.toLowerCase();
 			cleanName = cleanName.replace(' ', '');
 
-			$('#services').append('<div class="list-group-item">'+
-				'<span class="badge '+ clas + '">' + text + '</span>' +
+			$('#services').append('<div class="list-group-item">' +
+				'<span class="badge ' + clas + '">' + text + '</span>' +
 				'<a href="#" class="list-group-item-heading" onclick="\$\(\'\#' + cleanName + '\').toggleClass(\'collapse\');">' + name + '</a>' +
-				'<div id="' + cleanName + '" class="graph collapse">' + 
+				'<div id="' + cleanName + '" class="graph collapse">' +
 				'<canvas id="' + cleanName + '_cvs" width="400" height="150"></canvas>' +
-			   '</div>' +
+				'</div>' +
 				'</div>');
 
 			var gph_data = {
@@ -86,13 +86,13 @@ $(document).ready(function() {
 				scales: {
 					yAxes: [{
 						ticks: {
-							beginAtZero:true
+							beginAtZero: true
 						}
 					}]
 				}
 			};
 
-			item.response_times.forEach(function(datapoint) {
+			item.response_times.forEach(function (datapoint) {
 				gph_data.data.labels.push(datapoint.datetime);
 				gph_data.data.datasets[0].data.push(datapoint.value);
 			});
@@ -102,11 +102,26 @@ $(document).ready(function() {
 		});
 	};
 
-	$.getJSON( 'https://api.github.com/repos/' + uptimeConfig.github.org + '/' + uptimeConfig.github.repo + '/issues?state=all' ).done(message);
+	$.getJSON('https://api.github.com/repos/' + uptimeConfig.github.org + '/' + uptimeConfig.github.repo + '/issues?state=all').done(determineIncidentOrMaintenance);
+
+	var maintainIssues = [];
+	var incidentIssues = [];
+
+	function determineIncidentOrMaintenance(issues) {
+		issues.forEach(function (issue) {
+			if (issue.labels.length > 0) {
+				issue.labels.forEach(function (label) {
+					if (label.name == 'maintenance') maintainIssues.push(issue);
+					else incidentIssues.push(issue);
+				});
+			}
+		});
+		message(incidentIssues);
+	}
 
 	function message(issues) {
-		issues.forEach(function(issue) {
-			var status = issue.labels.reduce(function(status, label) {
+		issues.forEach(function (issue) {
+			var status = issue.labels.reduce(function (status, label) {
 				if (/^status:/.test(label.name)) {
 					return label.name.replace('status:', '');
 				} else {
@@ -114,15 +129,15 @@ $(document).ready(function() {
 				}
 			}, 'operational');
 
-			var systems = issue.labels.filter(function(label) {
+			var systems = issue.labels.filter(function (label) {
 				return /^system:/.test(label.name);
-			}).map(function(label) {
+			}).map(function (label) {
 				return label.name.replace('system:', '')
 			});
 
 			if (issue.state === 'open') {
 				$('#panel').data('incident', 'true');
-				$('#panel').attr('class', (status === 'operational' ? 'panel-success' : 'panel-warn') );
+				$('#panel').attr('class', (status === 'operational' ? 'panel-success' : 'panel-warn'));
 				$('#paneltitle').html('<a href="#incidents">' + issue.title + '</a>');
 			}
 
@@ -160,6 +175,12 @@ $(document).ready(function() {
 			html += '</div>';
 			html += '</article>';
 			$('#incidents').append(html);
+		});
+
+		maintainIssues.forEach(function (issue) {
+			$('#maintenance').append('<div class="list-group-item">' +
+				'<h2 class="list-group-item-heading">' + issue.title + '</h2>' +
+				'<p class="list-group-item-text">' + issue.body + '</p>');
 		});
 
 		function datetime(string) {

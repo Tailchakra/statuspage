ae24ac4f1fa980465a4efa8a7cd3f24622a192f3
vertig0ne <23177238+vertig0ne@users.noreply.github.com>
2018-03-05 21:57:03 +0000

    clean up code some add 5min refresh time

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	var config = {
+	const config = {
 		uptimerobot: {
 			api_keys: [
 				"m780064142-0c40e8dfe56e316d773f691f",
@@ -30,7 +30,7 @@ $(document).ready(function () {
 	};
 
 	const monitors = config.uptimerobot.api_keys;
-	for (var i in monitors) {
+	for (let i in monitors) {
 		var api_key = monitors[i];
 		$.post('https://api.uptimerobot.com/v2/getMonitors', {
 			"api_key": api_key,
@@ -40,28 +40,88 @@ $(document).ready(function () {
 			"all_time_uptime_ratio": config.uptimerobot.all_time_uptime_ratio,
 			"custom_uptime_ratios": config.uptimerobot.custom_uptime_ratios,
 			"response_times_average": config.uptimerobot.response_times_average
-		}, function (response) {
-			status(response);
-		}, 'json');
+		}, UptimeRobot, 'json');
 	}
 
-	function status(data) {
-		data.monitors = data.monitors.map(function (check) {
-			check.class = check.status === 2 ? 'label-success' : 'label-danger';
-			check.text = check.status === 2 ? 'operational' : 'major outage';
-			if (check.status !== 2 && !check.lasterrortime) {
-				check.lasterrortime = Date.now();
+	function _uptimeRobotSetStatus(check) {
+		check.class = check.status === 2 ? 'label-success' : 'label-danger';
+		check.text = check.status === 2 ? 'operational' : 'major outage';
+		if (check.status !== 2 && !check.lasterrortime) {
+			check.lasterrortime = Date.now();
+		}
+		if (check.status === 2 && Date.now() - (check.lasterrortime * 1000) <= 86400000) {
+			check.class = 'label-danger';
+			check.text = 'major outage';
+		}
+		if (check.status === 2 && Math.round(check.average_response_time) >= config.uptimerobot.response_times_warning) {
+			check.class = 'label-warning';
+			check.text = 'degraded performance';
+		}
+		return check;
+	}
+
+	function _uptimeRobotSetData(monitor) {
+		const clean_name = monitor.friendly_name.replace(/[^0-9a-zA-Z ]/g, '').replace(/ /g, '');
+		const uptime_ratio = monitor.custom_uptime_ratio.split('-');
+		const uptimeForever = monitor.all_time_uptime_ratio;
+
+		$('#services').append('<div class="list-group-item">' +
+			'<span class="badge ' + monitor.class + '">' + monitor.text + '</span>' +
+			'<a href="#" class="list-group-item-heading" onclick="\$\(\'\#' + monitor.clean_name + '\').toggleClass(\'collapse\');">' + monitor.friendly_name + '</a>' +
+			'<div id="' + monitor.clean_name + '" class="graph collapse">' +
+			'<canvas id="' + monitor.clean_name + '_cvs" width="400" height="150"></canvas>' +
+			'</div>' +
+			'</div>');
+	}
+
+	function _uptimeRobotSetGraph(monitor) {
+		$('#statistics tbody').append('<tr>' +
+			'<td>' + monitor.friendly_name + '</td>' +
+			'<td>' + monitor.uptime_ratio[0] + '%</td>' +
+			'<td>' + monitor.uptime_ratio[1] + '%</td>' +
+			'<td>' + monitor.uptime_ratio[2] + '%</td>' +
+			'<td>' + monitor.uptime_ratio[3] + '%</td>' +
+			'<td>' + monitor.uptime_ratio[4] + '%</td>' +
+			'<td>' + monitor.average_response_time + '</td>' +
+			'</tr>');
+
+		const gph_data = {
+			type: 'line',
+			data: {
+				labels: [],
+				datasets: [{
+					label: 'Response Time (ms)',
+					backgroundColor: "rgba(102,67,220,0.5)",
+					data: [],
+				}]
+			},
+			options: {
+				scales: {
+					xAxes: [{
+						display: false,
+						ticks: {
+							display: false,
+							scaleFontSize: 0
+						}
+					}]
+				}
 			}
-			if (check.status === 2 && Date.now() - (check.lasterrortime * 1000) <= 86400000) {
-				check.class = 'label-danger';
-				check.text = 'major outage';
-			}
-			if (check.status === 2 && Math.round(check.average_response_time) >= config.uptimerobot.response_times_warning) {
-				check.class = 'label-warning';
-				check.text = 'degraded performance';
-			}
-			return check;
+		};
+
+		monitor.response_times.forEach(function (datapoint) {
+			gph_data.data.labels.push(formatDate(new Date(datapoint.datetime * 1000), 'D d M Y H:i:s (T)'));
+			gph_data.data.datasets[0].data.push(datapoint.value);
 		});
+
+		gph_data.data.labels = gph_data.data.labels.reverse();
+		gph_data.data.datasets[0].data = gph_data.data.datasets[0].data.reverse();
+
+		const gph_ctx = $('#' + monitor.clean_name + '_cvs');
+		const gph = new Chart(gph_ctx, gph_data);
+	}
+
+	function UptimeRobot(data) {
+		data.monitors = data.monitors.map(_uptimeRobotSetStatus);
 
 		var status = data.monitors.reduce(function (status, check) {
 			return check.status !== 2 ? 'danger' : 'operational';
@@ -71,82 +131,26 @@ $(document).ready(function () {
 			$('#panel').attr('class', 'panel-success');
 			$('#paneltitle').html('All systems are operational.');
 		}
+
 		data.monitors.forEach(function (item) {
-			var name = item.friendly_name;
-			var clas = item.class;
-			var text = item.text;
-			var cleanName = item.friendly_name.replace(/[^0-9a-zA-Z ]/g, '');
-			cleanName = cleanName.replace(/ /g,'');
-
-			$('#services').append('<div class="list-group-item">' +
-				'<span class="badge ' + clas + '">' + text + '</span>' +
-				'<a href="#" class="list-group-item-heading" onclick="\$\(\'\#' + cleanName + '\').toggleClass(\'collapse\');">' + name + '</a>' +
-				'<div id="' + cleanName + '" class="graph collapse">' +
-				'<canvas id="' + cleanName + '_cvs" width="400" height="150"></canvas>' +
-				'</div>' +
-				'</div>');
-			
-			var upt = item.custom_uptime_ratio.split('-');
-			var uptimeForever = item.all_time_uptime_ratio;
-
-			$('#statistics tbody').append('<tr>' +
-			'<td>' + item.friendly_name + '</td>' +
-			'<td>' + upt[0] + '%</td>' +
-			'<td>' + upt[1] + '%</td>' +
-			'<td>' + upt[2] + '%</td>' +
-			'<td>' + upt[3] + '%</td>' +
-			'<td>' + uptimeForever + '%</td>' +
-			'<td>' + item.average_response_time + '</td>' +
-			'</tr>');
-
-			var gph_data = {
-				type: 'line',
-				data: {
-					labels: [],
-					datasets: [{
-						label: 'Response Time (ms)',
-						backgroundColor: "rgba(102,67,220,0.5)",
-						data: [],
-					}]
-				},
-				options: {
-					scales: {
-						xAxes: [
-							{
-								display: false,
-								ticks: {
-									display: false,
-									scaleFontSize: 0
-								}
-							}
-						]
-					}
-				}
-			};
-
-			item.response_times.forEach(function (datapoint) {
-				gph_data.data.labels.push(formatDate(new Date(datapoint.datetime * 1000),'D d M Y H:i:s (T)'));
-				gph_data.data.datasets[0].data.push(datapoint.value);
-			});
-
-			gph_data.data.labels = gph_data.data.labels.reverse();
-			gph_data.data.datasets[0].data = gph_data.data.datasets[0].data.reverse();
-
-			var gph_ctx = $('#' + cleanName + '_cvs');
-			var gph = new Chart(gph_ctx, gph_data);
+			item.clean_name = item.friendly_name.replace(/[^0-9a-zA-Z ]/g, '').replace(/ /g, '');
+			item.uptime_ratio = item.custom_uptime_ratio.split('-');
+			item.uptime_ratio.push(item.all_time_uptime_ratio);
+			_uptimeRobotSetData(item);
+			_uptimeRobotSetGraph(item);
 		});
 	};
 
-	$.getJSON('https://api.github.com/repos/' + config.github.org + '/' + config.github.repo + '/issues?state=all').done(determineIncidentOrMaintenance);
+	$.getJSON('https://api.github.com/repos/' + config.github.org + '/' + config.github.repo + '/issues?state=all').done(GitHubEntry);
 
 	var maintainIssues = [];
 	var incidentIssues = [];
 
-	function determineIncidentOrMaintenance(issues) {
+	function GitHubEntry(issues) {
 		issues.forEach(function (issue) {
 			if (issue.labels.length > 0) {
 				issue.labels.forEach(function (label) {
-					if (label.name == 'maintenance') maintainIssues.push(issue);
+					if (label.name == 'maintenance' && issue.state == 'open') maintainIssues.push(issue);
 					else incidentIssues.push(issue);
 				});
 			}
@@ -186,7 +190,7 @@ $(document).ready(function () {
 			}
 
 			html += '<div class="timeline-label">\n';
-			html += '<span class="date">' + formatDate(new Date(issue.created_at),'D d M Y H:i:s (T)') + '</span>\n';
+			html += '<span class="date">' + formatDate(new Date(issue.created_at), 'D d M Y H:i:s (T)') + '</span>\n';
 
 			if (issue.state === 'closed') {
 				html += '<span class="badge label-success pull-right">closed</span>';
@@ -203,7 +207,7 @@ $(document).ready(function () {
 			html += '<p>' + issue.body + '</p>\n';
 
 			if (issue.state === 'closed') {
-				html += '<p><em>Updated ' + formatDate(new Date(issue.closed_at),'D d M Y H:i:s (T)') + '<br/>';
+				html += '<p><em>Updated ' + formatDate(new Date(issue.closed_at), 'D d M Y H:i:s (T)') + '<br/>';
 				html += 'The system is back in normal operation.</p>';
 			}
 			html += '</div>';
@@ -235,43 +239,43 @@ $(document).ready(function () {
 		var fullDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 		var suffix = ['st', 'nd', 'rd', 'th'];
 		var z = {
-			a: (x.getHours() >= 12)? 'pm' : 'am',
-			A: (x.getHours() >= 12)? 'PM' : 'AM',
+			a: (x.getHours() >= 12) ? 'pm' : 'am',
+			A: (x.getHours() >= 12) ? 'PM' : 'AM',
 			B: Math.floor((((x.getUTCHours() + 1) % 24) + x.getUTCMinutes() / 60 + x.getUTCSeconds() / 3600) * 1000 / 24),
 			c: x.toISOString(),
-			m: (x.getHours().toString().length == 2)? x.getMonth() + 1 : '0' +  x.getMonth() + 1,
+			m: (x.getHours().toString().length == 2) ? x.getMonth() + 1 : '0' + x.getMonth() + 1,
 			M: months[x.getMonth()],
-			n:  x.getMonth() + 1,
+			n: x.getMonth() + 1,
 			L: parseInt(((x.getFullYear() % 4 == 0) && (x.getFullYear() % 100 != 0)) || (x.getFullYear() % 400 == 0)),
 			F: fullMonths[x.getMonth()],
-			d: (x.getDate().toString().length == 2)? x.getDate() : '0' + x.getDate(),
+			d: (x.getDate().toString().length == 2) ? x.getDate() : '0' + x.getDate(),
 			j: x.getDate(),
 			D: days[x.getDay()],
 			l: fullDays[x.getDay()],
 			N: x.getDay() + 1,
 			w: x.getDay(),
-			h: (x.getHours().toString().length == 2)? ((x.getHours() + 11) % 12 + 1) : '0' + ((x.getHours() + 11) % 12 + 1),
-			H: (x.getHours().toString().length == 2)? x.getHours() : '0' + x.getHours(),
+			h: (x.getHours().toString().length == 2) ? ((x.getHours() + 11) % 12 + 1) : '0' + ((x.getHours() + 11) % 12 + 1),
+			H: (x.getHours().toString().length == 2) ? x.getHours() : '0' + x.getHours(),
 			G: x.getHours(),
 			g: ((x.getHours() + 11) % 12 + 1),
 			O: x.toString().match(/([-\+][0-9]+)\s/)[1],
-			i: (x.getMinutes().toString().length == 2)? x.getMinutes() : '0' + x.getMinutes(),
-			s: (x.getSeconds().toString().length == 2)? x.getSeconds() : '0' + x.getSeconds(), 
-			T: x.toString().replace(/.*[(](.*)[)].*/,'$1'),
-			e: x.toString().replace(/.*[(](.*)[)].*/,'$1'),
+			i: (x.getMinutes().toString().length == 2) ? x.getMinutes() : '0' + x.getMinutes(),
+			s: (x.getSeconds().toString().length == 2) ? x.getSeconds() : '0' + x.getSeconds(),
+			T: x.toString().replace(/.*[(](.*)[)].*/, '$1'),
+			e: x.toString().replace(/.*[(](.*)[)].*/, '$1'),
 			Y: x.getFullYear(),
 			y: x.getYear(),
 			u: 000000,
 			v: 000000,
-			z: Math.round((new Date().setHours(23) - new Date(x.getYear()+1900, 0, 1, 0, 0, 0))/1000/60/60/24) - 1,
+			z: Math.round((new Date().setHours(23) - new Date(x.getYear() + 1900, 0, 1, 0, 0, 0)) / 1000 / 60 / 60 / 24) - 1,
 			U: Math.round(x.getTime() / 1000),
 		};
-		y = y.replace(/(a+|A+|B+|c+|m+|M+|n+|L+|F+|d+|D+|j+|l+|n+|N+|w+|g+|G+|O+|e+|u+|v+|z+|U+|h+|H+|i+|s+|T+|Y+|y+)/g, function(v) {
+		y = y.replace(/(a+|A+|B+|c+|m+|M+|n+|L+|F+|d+|D+|j+|l+|n+|N+|w+|g+|G+|O+|e+|u+|v+|z+|U+|h+|H+|i+|s+|T+|Y+|y+)/g, function (v) {
 			var t = eval('z.' + v.slice(-1));
 			return t;
 		});
-	
-		return y.replace(/(y+)/g, function(v) {
+
+		return y.replace(/(y+)/g, function (v) {
 			return x.getFullYear().toString().slice(-v.length)
 		});
 	};
